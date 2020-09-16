ae19abb82533374904727f3af914ea86a3299729
saumyabhatnagar1 <bhatnagar.saumya2000@gmail.com>
2020-09-16 17:05:26 +0530

    [Saumya]Setting up the front end

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ClubComponent} from './club/club.component'
+import { GalleryComponent } from './gallery/gallery.component';
+import {HomeComponent} from './home/home.component'
+import { TeamsComponent } from './teams/teams.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'clubs',component:ClubComponent},
+  {path:'team',component:TeamsComponent},
+  {path:'gallery',component:GalleryComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TeamsComponent } from './teams/teams.component';
+import { ClubComponent } from './club/club.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TeamsComponent,
+    ClubComponent,
+    GalleryComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
