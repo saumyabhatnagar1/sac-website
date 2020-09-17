995db82bff033d08b225ce1670997886104b725f
Himanshu Patel <hpatel27299@gmail.com>
2020-09-17 18:40:03 +0530

    [Himanshu] Made changes to the Header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {ClubComponent} from './club/club.component'
+import { ClubComponent } from './club/club.component'
 import { GalleryComponent } from './gallery/gallery.component';
-import {HomeComponent} from './home/home.component'
+import { HomeComponent } from './home/home.component'
 import { TeamsComponent } from './teams/teams.component';
 
 const routes: Routes = [
-  {path:'',component:HomeComponent},
-  {path:'clubs',component:ClubComponent},
-  {path:'team',component:TeamsComponent},
-  {path:'gallery',component:GalleryComponent},
+  {path: '', component: HomeComponent},
+  {path: 'team', component: TeamsComponent},
+  {path: 'clubs', component: ClubComponent},
+  {path: 'gallery', component: GalleryComponent},
 ];
 
 @NgModule({
