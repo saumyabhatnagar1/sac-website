9dca92d1f5b4ab904731473f9940efe3c3b0aa81
Himanshu Patel <hpatel27299@gmail.com>
2021-01-28 12:42:02 +0530

    [Himanshu] Updated hackathon page (JS part is broken)

--- src/app/special/special.component.ts
+++ src/app/special/special.component.ts
@@ -3,7 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-special',
   templateUrl: './special.component.html',
-  styleUrls: ['./special.component.css']
+  styleUrls: [
+    './special.component.css',
+    "../../assets/hackathon/fonts/line-icons.css",
+    "../../assets/hackathon/css/nivo-lightbox.css",
+    "../../assets/hackathon/css/animate.css",
+    "../../assets/hackathon/css/main.css",
+    "../../assets/hackathon/css/responsive.css"
+  ]
 })
 export class SpecialComponent implements OnInit {
 
